b3d77fbf6c6beebae02468b73bc9d84e3b52328e
menghan7 <han.meng.cs@gmail.com>
2015-01-25 15:24:38 -0500

    movsim substat added.

---
+++ Movsim_SMC_Substate/src/Movsim_SubState/MovSimSubState.java
@@ -0,0 +1,37 @@
+package Movsim_SubState;
+
+import javax.xml.bind.JAXBException;
+
+import movsimSMC.MovsimWrap;
+
+import org.xml.sax.SAXException;
+
+public class MovSimSubState extends MovSimFullState{
+	private final int subIndex;
+	
+	public MovSimSubState(double stepLength, int subindex) throws JAXBException, SAXException {
+		super(stepLength);
+		// TODO Auto-generated constructor stub
+		subIndex = subindex;
+	}
+	
+	public MovSimSubState(MovSimFullState fullState, int subindex) {
+		super(fullState.getMovSimWrap());
+		// TODO Auto-generated constructor stub
+		subIndex = subindex;
+	}
+	
+	@Override
+	public MovSimSubState clone() {
+		// TODO Auto-generated method stub
+		// MovSimSubState sub = (MovSimSubState)super.clone();
+		// subindex = this.subIndx;
+		return (MovSimSubState)super.clone();
+	}
+
+	public MovSimSubState(MovsimWrap movsimPF, int subindex) {
+		super(movsimPF);
+		// TODO Auto-generated constructor stub
+		subIndex = subindex;
+	}
+}
